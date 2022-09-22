e4a88a8b7b0b6d01100ac8345843418db999a791
Ubuntu <ubuntu@ip-172-31-45-245.ap-southeast-1.compute.internal>
2022-09-22 23:52:09 +0800

    first commit

---
+++ .terraform.lock.hcl
@@ -0,0 +1,24 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/confluentinc/confluent" {
+  version     = "1.4.0"
+  constraints = "1.4.0"
+  hashes = [
+    "h1:aQnIxqHLSVbPqDQ//Om+DLa5HvuI5Jl+TtMRKFA5khU=",
+    "zh:3d900f8399986ccff737f3b25527679acd32f33fbabede601791da024b0e98c0",
+    "zh:435d03dcd0a507c0e6f228f9871c6c66ff525b03991e45d3925948e7fb03223d",
+    "zh:4a5b38eee0520660432bd7e35f85d082a19cd817027bc5159357f90b7d14bd01",
+    "zh:513425827eb5c9d14d56e0413af9a2483a82cb660e9ccc824e894f541811475a",
+    "zh:567cfc369577bf66903bfbce2e81de9a05b5f92ca1b4c009cf1c7c393f278ba7",
+    "zh:5a61ec50d83aa709a78ae42c745c2fb02142b94af606ec7220efacaad5682b12",
+    "zh:8846a409d0663081d897503fd5bc8e4f2de1549f06fc199bcf8482decb006920",
+    "zh:a0f74148a84d34153064525da6253062cdb5f2ad093ba396601466fda406b775",
+    "zh:b721b075c9713c60dba0f17130d6c39afc8b5eb73781bbdd8773f53274571cdb",
+    "zh:ba0f4fbcad05d3629d72c94de527f884232c39f6a8518febfb5630f7a62fc6bb",
+    "zh:c57a049fccfabc33747775173a8fe55c06ec6f8a168b45fa69a9f5f2894eae6c",
+    "zh:c727eb32fb93bfbfe3490b451ed1ecbd4ed3474b9c1abd3134cb1b906f9ceefa",
+    "zh:d3d8ffc80570e4ed20f297d01129fd7d4c998f7de36b4f84e54534e5bd75a50e",
+    "zh:d50fd9cb9c2d4d10083ea08cac639c7c08f90742d34a551ff8d80d02bd1b9a4d",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,113 @@
+terraform {
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.4.0"
+    }
+  }
+}
+
+/*provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}*/
+
+resource "confluent_environment" "test_env" {
+  display_name = "test_env"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "confluent_kafka_cluster" "basic" {
+  display_name = "Inventory"
+  availability = "SINGLE_ZONE"
+  cloud        = "AWS"
+  region       = "ap-southeast-1"
+  basic {}
+  environment {
+    id = confluent_environment.test_env.id
+  }
+}
+
+data "confluent_user" "rvoon" {
+  id = "u-xmop8q"
+}
+
+resource "confluent_role_binding" "rvoon-cloud-cluster-admin" {
+  principal   = "User:${data.confluent_user.rvoon.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.basic.rbac_crn
+}
+
+resource "confluent_api_key" "rvoon-kafka-api-key" {
+  display_name = "rvoon-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'Ryan Voon' user account"
+  owner {
+    id          = data.confluent_user.rvoon.id
+    api_version = data.confluent_user.rvoon.api_version
+    kind        = data.confluent_user.rvoon.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.basic.id
+    api_version = confluent_kafka_cluster.basic.api_version
+    kind        = confluent_kafka_cluster.basic.kind
+
+    environment {
+      id = confluent_environment.test_env.id
+    }
+  }
+  depends_on = [
+    confluent_role_binding.rvoon-cloud-cluster-admin
+  ]
+}
+
+resource "confluent_kafka_topic" "orders" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  topic_name    = "orders"
+  partitions_count = 1
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key = confluent_api_key.rvoon-kafka-api-key.id
+    secret = confluent_api_key.rvoon-kafka-api-key.secret
+  }
+}
+
+resource "confluent_kafka_acl" "rvoon-read-on-topic" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  resource_type = "TOPIC"
+  resource_name = confluent_kafka_topic.orders.topic_name
+  pattern_type  = "LITERAL"
+  principal     = "User:${data.confluent_user.rvoon.id}"
+  host          = "*"
+  operation     = "READ"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key = confluent_api_key.rvoon-kafka-api-key.id
+    secret = confluent_api_key.rvoon-kafka-api-key.secret
+  }
+}
+
+resource "confluent_kafka_acl" "rvoon-read-on-group" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  resource_type = "GROUP"
+  resource_name = "consumer_"
+  pattern_type  = "PREFIXED"
+  principal     = "User:${data.confluent_user.rvoon.id}"
+  host          = "*"
+  operation     = "READ"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key = confluent_api_key.rvoon-kafka-api-key.id
+    secret = confluent_api_key.rvoon-kafka-api-key.secret
+  }
+}
