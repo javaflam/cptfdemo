91c5c19a73becb56a2ba65333bfc2d075b50810d
javaflam <voonck@gmail.com>
2022-09-24 00:34:17 +0800

    revised config

--- main.tf
+++ main.tf
@@ -25,23 +25,24 @@ resource "confluent_kafka_cluster" "basic" {
   }
 }
 
-data "confluent_user" "rvoon" {
-  id = "u-xmop8q"
+resource "confluent_service_account" "app-manager" {
+  display_name = "app-manager"
+  description  = "Service account to manage 'inventory' Kafka cluster"
 }
 
-resource "confluent_role_binding" "rvoon-cloud-cluster-admin" {
-  principal   = "User:${data.confluent_user.rvoon.id}"
+resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
+  principal   = "User:${confluent_service_account.app-manager.id}"
   role_name   = "CloudClusterAdmin"
   crn_pattern = confluent_kafka_cluster.basic.rbac_crn
 }
 
-resource "confluent_api_key" "rvoon-kafka-api-key" {
-  display_name = "rvoon-kafka-api-key"
-  description  = "Kafka API Key that is owned by 'Ryan Voon' user account"
+resource "confluent_api_key" "app-manager-kafka-api-key" {
+  display_name = "app-manager-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-manager' user account"
   owner {
-    id          = data.confluent_user.rvoon.id
-    api_version = data.confluent_user.rvoon.api_version
-    kind        = data.confluent_user.rvoon.kind
+    id          = confluent_service_account.app-manager.id
+    api_version = confluent_service_account.app-manager.api_version
+    kind        = confluent_service_account.app-manager.kind
   }
 
   managed_resource {
@@ -54,7 +55,7 @@ resource "confluent_api_key" "rvoon-kafka-api-key" {
     }
   }
   depends_on = [
-    confluent_role_binding.rvoon-cloud-cluster-admin
+    confluent_role_binding.app-manager-kafka-cluster-admin
   ]
 }
 
@@ -66,43 +67,111 @@ resource "confluent_kafka_topic" "orders" {
   partitions_count = 1
   rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
   credentials {
-    key = confluent_api_key.rvoon-kafka-api-key.id
-    secret = confluent_api_key.rvoon-kafka-api-key.secret
+    key = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
   }
 }
 
-resource "confluent_kafka_acl" "rvoon-read-on-topic" {
+resource "confluent_service_account" "app-producer" {
+  display_name = "app-producer"
+  description  = "Service account to produce to 'orders' topic of 'inventory' Kafka cluster"
+}
+
+resource "confluent_api_key" "app-producer-kafka-api-key" {
+  display_name = "app-producer-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-producer' service account"
+  owner {
+    id          = confluent_service_account.app-producer.id
+    api_version = confluent_service_account.app-producer.api_version
+    kind        = confluent_service_account.app-producer.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.basic.id
+    api_version = confluent_kafka_cluster.basic.api_version
+    kind        = confluent_kafka_cluster.basic.kind
+
+    environment {
+      id = confluent_environment.tfenv.id
+    }
+  }
+}
+
+resource "confluent_kafka_acl" "app-producer-write-on-topic" {
   kafka_cluster {
     id = confluent_kafka_cluster.basic.id
   }
   resource_type = "TOPIC"
   resource_name = confluent_kafka_topic.orders.topic_name
   pattern_type  = "LITERAL"
-  principal     = "User:${data.confluent_user.rvoon.id}"
+  principal     = "User:${confluent_service_account.app-producer.id}"
+  host          = "*"
+  operation     = "WRITE"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+resource "confluent_service_account" "app-consumer" {
+  display_name = "app-consumer"
+  description  = "Service account to consume from 'orders' topic of 'inventory' Kafka cluster"
+}
+
+resource "confluent_api_key" "app-consumer-kafka-api-key" {
+  display_name = "app-consumer-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-consumer' service account"
+  owner {
+    id          = confluent_service_account.app-consumer.id
+    api_version = confluent_service_account.app-consumer.api_version
+    kind        = confluent_service_account.app-consumer.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.basic.id
+    api_version = confluent_kafka_cluster.basic.api_version
+    kind        = confluent_kafka_cluster.basic.kind
+
+    environment {
+      id = confluent_environment.tfenv.id
+    }
+  }
+}
+
+resource "confluent_kafka_acl" "app-consumer-read-on-topic" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  resource_type = "TOPIC"
+  resource_name = confluent_kafka_topic.orders.topic_name
+  pattern_type  = "LITERAL"
+  principal     = "User:${confluent_service_account.app-consumer.id}"
   host          = "*"
   operation     = "READ"
   permission    = "ALLOW"
   rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
   credentials {
-    key = confluent_api_key.rvoon-kafka-api-key.id
-    secret = confluent_api_key.rvoon-kafka-api-key.secret
+    key = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
   }
 }
 
-resource "confluent_kafka_acl" "rvoon-read-on-group" {
+resource "confluent_kafka_acl" "app-consumer-read-on-group" {
   kafka_cluster {
     id = confluent_kafka_cluster.basic.id
   }
   resource_type = "GROUP"
   resource_name = "consumer_"
   pattern_type  = "PREFIXED"
-  principal     = "User:${data.confluent_user.rvoon.id}"
+  principal     = "User:${confluent_service_account.app-consumer.id}"
   host          = "*"
   operation     = "READ"
   permission    = "ALLOW"
   rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
   credentials {
-    key = confluent_api_key.rvoon-kafka-api-key.id
-    secret = confluent_api_key.rvoon-kafka-api-key.secret
+    key = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
   }
 }
